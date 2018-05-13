e96c34650ac2c8a2f2b86ef338e4c6f734247120
basblu <bloombased@gmail.com>
2018-05-14 01:51:28 +0200

    Update Initializer.py

--- ex3/Initializer.py
+++ ex3/Initializer.py
@@ -5,4 +5,6 @@ class Initializer():
     def initialize(self,task):
         task_matrix = task.get_task_matrix()
         len = task_matrix.shape[0]
-        return np.ones((len,len))
+        return np.ones((len,len)) 
+    
+    #hi
