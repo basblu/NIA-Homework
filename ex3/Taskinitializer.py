01f2e5e12ff9b81a2826eef787eef04c1c9e250a
basblu <bloombased@gmail.com>
2018-05-14 02:18:07 +0200

    Update Taskinitializer.py

--- ex3/Taskinitializer.py
+++ ex3/Taskinitializer.py
@@ -1,12 +1,13 @@
-from typing import List, Any
+#from typing import List, Any
 import numpy as np
 import os
 
 #this class initializes one of the 3 given tasks using the corresponding TSP-Distances-File in ./TSPs/
 
+
 class Taskinitializer():
 
-    def __init__(self,task):
+    def __init__(self, task):
 
         if task == 1:
             self.file = '01.tsp'
@@ -18,7 +19,7 @@ class Taskinitializer():
         self.task_matrix = self.get_task_matrix()
 
     def get_task_matrix(self):
-        with open('.'+os.sep+'TSPs'+os.sep+self.file, 'r') as filereader:
+        with open('TSPs/'+self.file) as filereader:
                 task_matrix = []
                 for line in filereader:
                     row = line.split(' ')
@@ -28,5 +29,10 @@ class Taskinitializer():
                     task_matrix.append(row)
         return np.asarray(task_matrix)
 
-    def solution_evaluation(self,solution):
-        return np.sum(self.get_task_matrix()[solution[0:-1], solution[1:]])
+    def solution_evaluator(self, solution):
+
+        task_matrix = self.get_task_matrix()
+
+
+        return np.sum(task_matrix[solution[0:-1], solution[1:]])
+
