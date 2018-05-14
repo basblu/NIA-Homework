27ba34d39283c28c4399f189d1a990066421cd6d
basblu <bloombased@gmail.com>
2018-05-14 02:16:09 +0200

    Update SolutionGenerator.py

--- ex3/SolutionGenerator.py
+++ ex3/SolutionGenerator.py
@@ -7,10 +7,9 @@ class SolutionGenerator():
         self.num_of_ants = num_of_ants
         self.alpha = alpha
         self.beta = beta
-<<<<<<< HEAD
+
         self.task_matrix = task.get_task_matrix()
-=======
->>>>>>> bb7e0cb35033eb64e55f073769caf3537d169aba
+
 
     def set_task(self, task):
         self.task = task
@@ -18,7 +17,7 @@ class SolutionGenerator():
 
     def generate_solution(self, pheromone_matrix):
         num_citys = self.task_matrix.shape[0]
-<<<<<<< HEAD
+
         cities = np.arange(0,num_citys,1)
 
 
@@ -42,25 +41,24 @@ class SolutionGenerator():
 
         cities = np.delete(cities,city)
 
-=======
         cities = np.arange(1,num_citys,1)
         eta_matrix = 1 / self.task_matrix
 
         city = np.random.choice(cities,1,False)
         
         cities = cities.delete(city) #needs an index or something (you ment delete and not remove, right?)
->>>>>>> bb7e0cb35033eb64e55f073769caf3537d169aba
+
         solution = np.array([city])
 
 
         for c in range(num_citys-1):
 
             #berechne die Wahrscheinlichkeiten von city zu allen mÃ¶glichen city_next zu gelangen
-<<<<<<< HEAD
-=======
+
+
             n = np.power(pheromone_matrix[city, cities], self.alpha)*np.power(eta_matrix[city,cities],self.beta)
             d = np.sum(np.power(pheromone_matrix[city, cities], self.alpha)*np.power(eta_matrix[city,cities],self.beta))
->>>>>>> bb7e0cb35033eb64e55f073769caf3537d169aba
+
 
             n = np.power(pheromone_matrix[city, cities], self.alpha)*np.power(eta_matrix[city,cities],self.beta)
 
@@ -77,13 +75,13 @@ class SolutionGenerator():
             #print('solution: ',solution)
             
             #entferne city aus der liste
-<<<<<<< HEAD
+
             for i, c in enumerate(cities):
                 if c == city:
                     np.delete(cities, i)
-=======
+
             cities = cities.delete(city_next) #needs an index or something
->>>>>>> bb7e0cb35033eb64e55f073769caf3537d169aba
+
 
             city = city_next
 
@@ -92,7 +90,7 @@ class SolutionGenerator():
 
     def collecting_solutions(self,pheromone_matrix):
 
-<<<<<<< HEAD
+
         solutions = list()
         print(solutions)
         evaluations = list()
@@ -105,14 +103,14 @@ class SolutionGenerator():
             evaluation = self.task.solution_evaluator(solutions[ant])
 
             evaluations.append(evaluation)
-=======
+
         solutions = np.array() #gives me an error, needs a parameter [list() should work, but well]
         evaluations = np.array()
 
         for ant in range(self.num_of_ants):
             solutions.append(self.generate_solution(pheromone_matrix))
             evaluations.append(self.task.solution_evaluation(solutions[ant]))
->>>>>>> bb7e0cb35033eb64e55f073769caf3537d169aba
+
 
 
         indices = np.argsort(evaluations)
